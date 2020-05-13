9d1e404b563117fabeca2c13970caed8e553d637
StephenAgui <guijing2@163.com>
2020-05-14 00:26:56 +0800

    添加状态机

--- week05/client.js
+++ week05/client.js
@@ -29,7 +29,7 @@ ${this.bodyText}`;
 
     send(connection){
         return new Promise((resolve,reject)=>{
-            
+            const parser = new ResponseParser();
             
             if(connection){
                 connection.write(this.toString());
@@ -45,7 +45,9 @@ ${this.bodyText}`;
             }
             connection.on("data",data=>{
                 console.log('received from serve:');
-                resolve(data.toString());
+                // resolve(data.toString());
+                parser.receive(data.toString())
+          console.log(parser.response,parser.isFinished, 'headers')
                 connection.end();
             });
             connection.on("error",error=>{
@@ -62,6 +64,143 @@ ${this.bodyText}`;
 class Response {
 
 }
+
+class ResponseParser{
+    constructor() {
+        this.WAITING_STATUS_LINE = 0
+        this.WAITING_STATUS_LINE_END = 1
+        this.WAITING_HEADER_NAME = 2
+        this.WAITING_HEADER_SPACE = 3
+        this.WAITING_HEADER_VALUE = 4
+        this.WAITING_HEADER_LINE_END = 5
+        this.WAITING_HEADER_BLOCK_END = 6
+        this.WAITING_BODY = 7
+        this.current = this.WAITING_STATUS_LINE
+        this.headers = {}
+        this.headerName = ''
+        this.headerValue = ''
+        this.statusLine = ''
+        this.bodyParser = null
+      }
+    
+      get isFinished() {
+        return this.bodyParser && this.bodyParser.isFinished
+      }
+    
+      get response() {
+        this.statusLine.match(/HTTP\/1.1 ([0-9]+) ([\s\S]+)/)
+        return {
+          statusCode: RegExp.$1,
+          statusText: RegExp.$2,
+          headers: this.headers,
+          body: this.bodyParser.content.join('')
+        }
+      }
+    
+      receive(string) {
+        for (let i = 0; i < string.length; i++) {
+          this.receiveChar(string.charAt(i))
+        }
+      }
+    
+      receiveChar(char) {
+        if (this.current === this.WAITING_STATUS_LINE) {
+          if (char === '\r') {
+            this.current = this.WAITING_STATUS_LINE_END
+          } else if (char === '\n') {
+            this.current = this.WAITING_HEADER_NAME
+          } else {
+            this.statusLine += char
+          }
+        } else if (this.current === this.WAITING_STATUS_LINE_END) {
+          if (char === '\n') {
+            this.current = this.WAITING_HEADER_NAME
+          }
+        } else if (this.current === this.WAITING_HEADER_NAME) {
+          if (char === ':') {
+            this.current = this.WAITING_HEADER_SPACE
+          } else if (char === '\r') {
+            this.current = this.WAITING_HEADER_BLOCK_END
+            if (this.headers['Transfer-Encoding'] === 'chunked') {
+              this.bodyParser = new TrunkedBodyParser()
+            }
+          } else {
+            this.headerName += char
+          }
+        } 
+        else if (this.current === this.WAITING_HEADER_BLOCK_END) {
+          if (char === '\n') {
+            this.current = this.WAITING_BODY
+          }
+        }else if (this.current === this.WAITING_HEADER_SPACE) {
+          if (char === ' ') {
+            this.current = this.WAITING_HEADER_VALUE
+          }
+        } else if (this.current === this.WAITING_HEADER_VALUE) {
+          if (char === '\r') {
+            this.current = this.WAITING_HEADER_LINE_END
+            this.headers[this.headerName] = this.headerValue
+            this.headerName = this.headerValue = ''
+          } else {
+            this.headerValue += char
+          }
+        } else if (this.current === this.WAITING_HEADER_LINE_END) {
+          if (char === '\n') {
+            this.current = this.WAITING_HEADER_NAME
+          }
+        } else if (this.current === this.WAITING_BODY) {
+          this.bodyParser.receiveChar(char)
+        }
+      }
+}
+
+
+class TrunkedBodyParser {
+    constructor() {
+      this.WAITING_LENGTH = 0
+      this.WAITING_LENGTH_LINE_END = 1
+      this.READING_TRUNK = 2
+      this.WAITING_NEW_LINE = 3
+      this.WAITING_NEW_LINE_END = 4
+      this.length = 0
+      this.content = []
+      this.isFinished = false
+      this.current = this.WAITING_LENGTH
+    }
+    receiveChar(char) {
+      if (this.current === this.WAITING_LENGTH) {
+        if (char === '\r') {
+          if (this.length === 0) {
+            this.isFinished = true
+          }
+          this.current = this.WAITING_LENGTH_LINE_END
+        } else {
+          this.length *= 10
+          this.length += char.charCodeAt(0) - '0'.charCodeAt(0)
+        }
+      } else if (this.current === this.WAITING_LENGTH_LINE_END) {
+        if (char === '\n') {
+          this.current = this.READING_TRUNK
+        }
+      } else if (this.current === this.READING_TRUNK) {
+        if(/[^\r\n]/.test(char)){
+          this.content.push(char)
+        }
+        this.length --
+        if (this.length === 0) {
+          this.current = this.WAITING_NEW_LINE
+        }
+      } else if (this.current === this.WAITING_NEW_LINE) {
+        if (char === '\r') {
+          this.current = this.WAITING_NEW_LINE_END
+        }
+      } else if (this.current === this.WAITING_NEW_LINE_END) {
+        if (char === '\n') {
+          this.current = this.WAITING_LENGTH
+        }
+      }
+    }
+  }
 
 void async function(){
     let request = new Request({
@@ -94,7 +233,7 @@ const client = net.createConnection({
 
         let request = new Request({
             methond:"POST",
-            prot:"8088",
+            port:"8088",
             host:"127.0.0.1",
             path:"/",
             body:{
