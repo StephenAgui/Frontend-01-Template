b797c95f05b531440df6c77db9e39fed82c1011f
StephenAgui <guijing2@163.com>
2020-05-20 23:38:55 +0800

    css 计算

---
+++ week06/match/match.js
@@ -0,0 +1,59 @@
+
+
+function start(c){
+    if(c === 'a'){
+        return foundA;
+    }else{
+        return start;
+    }
+}
+
+function match(string){
+    let state = start;
+    for(let c of string){
+        state = state(c);
+    }
+    return state === end;
+}
+
+function end(c){
+    return end;
+}
+
+function foundA(c){
+    if(c === 'b'){
+        return foundB;
+    }else{
+        return start(c);
+    }
+}
+
+function foundB(c){
+    if(c === 'c'){
+        return foundC;
+    }else{
+        return start(c);
+    }
+}
+
+function foundC(c){
+    if(c === 'd'){
+        return foundD;
+    }else{
+        return start(c);
+    }
+}
+
+function foundD(c){
+    if(c === 'e'){
+        return end;
+    }else{
+        return start(c);
+    }
+}
+
+
+console.log(match("abcd,abcd,abcdEddd"));
+console.log(match("abcdefg"));
+
+
